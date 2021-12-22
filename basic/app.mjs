c9e5df609f7137bb531415f2e7bceb3287564ff7
ianiiaannn <ianiiaannn@gmail.com>
2021-12-22 11:50:17 +0800

    basic flags

--- basic/app.mjs
+++ basic/app.mjs
@@ -11,6 +11,19 @@ app.get('/', (req, res)=>{
   res.send('target server');
 });
 
+app.get('/1', (req, res)=>{
+  res.render('basic', {script: '', message: 'hmmm where is the flag?<br><!-- FLAG{FIRST_LEVELLLLLLLLLLLLLLLLLLLLLL} -->'});
+});
+
+app.get('/2', (req, res)=>{
+  res.render('basic', {script: 'document.addEventListener(\'contextmenu\', event => {event.preventDefault();alert(\'No U little hacker\')});', message: 'no rightclick please<br><!-- FLAG{HOW_RUDE_YOU_ARE} -->'});
+});
+
+app.get('/3', (req, res)=>{
+  if(req.headers.referer=='')
+  res.render('basic', {script: 'document.addEventListener(\'contextmenu\', event => {event.preventDefault();alert(\'No U little hacker\')});', message: 'no rightclick please<br><!-- FLAG{HOW_RUDE_YOU_ARE} -->'});
+});
+
 app.listen(PORT, ()=>{
   console.log('app started on port '+PORT+'.');
 });

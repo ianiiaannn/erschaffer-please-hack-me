1ea7ff607bf5e034bb5968719d6f70d2e0962e7b
ianiiaannn <ianiiaannn@gmail.com>
2022-01-06 20:00:33 +0800

    database testing

--- judge/app.mjs
+++ judge/app.mjs
@@ -13,13 +13,44 @@ const pool=mariadb.createPool({
   user: 'root',
   password: 'serect',
   connectionLimit: '5',
+  database: 'judge'
 });
 pool.getConnection();
 
+async function dbLookup(q) {
+  let conn,message;
+  try {
+    conn = await pool.getConnection();
+    message = await conn.query(q);
+  } catch (err) {
+    console.log(err);
+    message = err;
+  } finally {
+    if (conn) conn.end();
+    return message;
+  }
+};
+
 app.get('/', (req, res)=>{
-  res.send('score server');
+  res.render('homepage');
+});
+
+app.post('/getlist', (req, res)=>{
+  res.send(dbLookup(''));
 });
 
 app.listen(PORT, ()=>{
   console.log('app started on port '+PORT+'.');
 });
+
+function dbInit(){
+  console.log(dbLookup`CREATE DATABASE IF NOT EXISTS \`judge\``);
+  console.log(dbLookup`use judge`);
+  console.log(dbLookup(`CREATE TABLE IF NOT EXISTS \`problems\` (
+    \`id\` int(6) unsigned NOT NULL,
+    \`content\` int(3) unsigned NOT NULL,
+    \`flag\` varchar(200) NOT NULL,
+    PRIMARY KEY (\`id\,\`rev\`)
+  ) DEFAULT CHARSET=utf8;`));
+}
+dbInit();
